cfd361234787fe1a073ff34b90265a170645cdec
Faria-Lorin <florin20@stuy.edu>
2021-01-10 16:02:18 -0500

    I used the start and end variales to find the size. I set a Node current to the start value, so if start is null that means that there is nothing in the list and the count stays 0. If current != null then count increases and if current is at the end of the list, meaning that getNext() would be null, then current becomes null and the loop stops, giving you the size of the LinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -6,5 +6,22 @@ public class MyLinkedList{
    end = null;
    size = 0;
  }
+ public int size(){
+   int count = 0;
+   Node current = start;
+   while (current != null){
+     count++;;
+     current = current.getNext();
+   }
+   return count;
+ }
+
+ /*
+ public boolean add(String value);
+ public void add(int index, String value);
+ public String get(int index);
+ public String set(int index, String value);
+ public String toString();
+ */
 
 }
