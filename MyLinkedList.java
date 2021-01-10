14f9826460422d989438939faf01faf2946e7b5c
Faria-Lorin <florin20@stuy.edu>
2021-01-10 16:18:12 -0500

    I didn't add an if statement at first, adding to an empty list was not possible, so I fixed that. since this new node/value will be added to the end of the list, no matter the size, next = null and the size will increase by 1. The difference is that when size = 0, you have to change the start value and the prev = null, but elsewise, the next node after n has to be set to a, and the prev of a is end. In both cases at the end of the code, end has to be set to a because it becomes the last node

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -16,12 +16,43 @@ public class MyLinkedList{
    return count;
  }
 
+ public boolean add(String value){
+  Node a = new Node(value);
+  a.setNext(null);
+  if (size == 0){
+    start = a;
+    end = a;
+    a.setPrev(null);
+  }
+ else {
+   end.setNext(a);
+   a.setPrev(end);
+   end = a;
+ }
+ size++;
+ return true;
+}
  /*
- public boolean add(String value);
- public void add(int index, String value);
+ public void add(int index, String value){
+  if (index >= size){
+  throw new IndexOutOfBoundsException("Index" + index + "is out of bounds")
+}
+}
  public String get(int index);
  public String set(int index, String value);
  public String toString();
  */
-
+public static void main(String[] args) {
+  MyLinkedList a = new MyLinkedList();
+  MyLinkedList b = new MyLinkedList();
+  for(int i = 0; i < 10; i++){
+    if(i < 5){
+      a.add(i+"");
+    }else{
+      b.add(i+"");
+    }
+  }
+  System.out.println(a.size());
+  System.out.println(b.size());
+}
 }
