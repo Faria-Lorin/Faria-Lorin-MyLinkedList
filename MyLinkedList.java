769e1fe7eaa45dc3262427693e83a43d6ca73204
Faria-Lorin <florin20@stuy.edu>
2021-01-10 19:34:55 -0500

    i fixed my remove method to include a condition for when size = 1, and I edited my toString methods so that the ending bracket is added outside the loop

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -113,13 +113,14 @@ public class MyLinkedList{
    Node current = start;
    for(int i = 0; i < size; i++){
      if (i == size - 1){
-       out += current.getData() + "]";
+       out += current.getData();
      }
      else{
        out += current.getData() + ", ";
      }
      current = current.getNext();
    }
+   out += "]";
    return out;
  }
 
@@ -129,13 +130,14 @@ public class MyLinkedList{
    Node current = end;
    for(int i = size; i > 0; i--){
      if (i == 1){
-       out += current.getData() + "]";
+       out += current.getData();
      }
      else{
        out += current.getData() + ", ";
      }
      current = current.getPrev();
    }
+   out += "]";
    return out;
  }
 
@@ -150,7 +152,11 @@ public class MyLinkedList{
      for (int i = 0; i <= index; i++){
       if (i == index){
         out += toRemove.getData();
-        if (index == 0){
+        if (size == 1){
+           start = null;
+           end = null;
+        }
+        else if (index == 0){
            start = toRemove.getNext();
            toRemove.setNext(null);
            start.setPrev(null);
@@ -177,7 +183,8 @@ public class MyLinkedList{
  }
  public static void main(String[] args) {
       MyLinkedList words = new MyLinkedList();
-
+      MyLinkedList size1 = new MyLinkedList();
+      size1.add("hi");
       //add 0-9 in order
       for(int i = 0; i < 10; i++){
         words.add(i+"");
@@ -203,11 +210,14 @@ public class MyLinkedList{
       }catch(IndexOutOfBoundsException e){
 
       }
-
-      words.remove(11);
+      System.out.println(size1.toString());
+      size1.remove(0);
+      words.remove(0);
+      System.out.println(size1.toString());
       System.out.println("Your result:\n"+words+" "+words.size());
       System.out.println("expected:\n[head, 0x, 0, 1x, 1, 2x, 2, 3x, 3, 4x, 4, 5x, 5, 6x, 6, 7x, 7, 8x, 8, 9x, 9, tail] 22");
       System.out.println("\nYour result:\n"+words.toStringReversed()+" "+words.size());
       System.out.println("expected:\n[tail, 9, 9x, 8, 8x, 7, 7x, 6, 6x, 5, 5x, 4, 4x, 3, 3x, 2, 2x, 1, 1x, 0, 0x, head] 22");
+
 }
 }
