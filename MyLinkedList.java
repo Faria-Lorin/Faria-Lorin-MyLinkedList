fe85f493be502e3d8570deb3ad55cd59240d59d1
Faria-Lorin <florin20@stuy.edu>
2021-01-10 19:04:04 -0500

    I added to my void add method to take into count if the size of the list being added to is 0, if a node is being added as the head or tail or if it's being added in the middle

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -37,22 +37,33 @@ public class MyLinkedList{
 
  public void add(int index, String value){
   size();
-  if (index >= size || index < 0){
+  if (index > size || index < 0){
     throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
   }
   else {
     Node temp = new Node(value);
     Node current = start;
-    for (int i = 0; i < index + 1; i++){
+    for (int i = 0; i <= index; i++){
      if (i == index){
-       Node toNext = current;
-       Node toPrev = current.getPrev();
-       temp.setPrev(toPrev);
-       temp.setNext(toNext);
-       current.setPrev(temp);
-       toPrev.setNext(temp);
+       if (size == 0 || index == size){
+         add(value);
+       }
+       else if (index == 0){
+          start.setPrev(temp);
+          temp.setPrev(null);
+          temp.setNext(start);
+          start = temp;
+       }
+       else {
+         Node toNext = current;
+         Node toPrev = current.getPrev();
+         toPrev.setNext(temp);
+         current.setPrev(temp);
+         temp.setPrev(toPrev);
+         temp.setNext(toNext);
+       }
      }
-     current = current.getNext();
+    else current = current.getNext();
    }
  }
  size ++;
@@ -126,21 +137,38 @@ public String get(int index){
  }
 
 public static void main(String[] args) {
-  MyLinkedList a = new MyLinkedList();
-  MyLinkedList b = new MyLinkedList();
-  for(int i = 0; i < 10; i++){
-    if(i < 5){
-      a.add(i+"h");
-    }else{
-      b.add(i+"");
-    }
-  }
-  a.add(3, "9");
-  System.out.println(a.toStringReversed());
-  System.out.println(a.toString());
-  System.out.println(a.set(3, "snow"));
-  System.out.println(a.get(3));
-  System.out.println(a.size());
-  System.out.println(b.size());
+      MyLinkedList words = new MyLinkedList();
+
+      //add 0-9 in order
+      for(int i = 0; i < 10; i++){
+        words.add(i+"");
+      }
+      //insert 0x - 9x before each value 0-9
+      for(int i = 0; i < 10; i++){
+        words.add(2*i,i+"x");
+      }
+      //check the head/tail add
+      words.add(0,"head");
+      words.add(words.size(),"tail");
+
+      try{
+        words.add(-1,"");
+        System.out.println("ERROR! Added to negative index");
+      }catch(IndexOutOfBoundsException e){
+
+      }
+
+      try{
+        words.add(words.size()+1,"");
+        System.out.println("ERROR! Added to (size + 1) index");
+      }catch(IndexOutOfBoundsException e){
+
+      }
+
+
+      System.out.println("Your result:\n"+words+" "+words.size());
+      System.out.println("expected:\n[head, 0x, 0, 1x, 1, 2x, 2, 3x, 3, 4x, 4, 5x, 5, 6x, 6, 7x, 7, 8x, 8, 9x, 9, tail] 22");
+      System.out.println("\nYour result:\n"+words.toStringReversed()+" "+words.size());
+      System.out.println("expected:\n[tail, 9, 9x, 8, 8x, 7, 7x, 6, 6x, 5, 5x, 4, 4x, 3, 3x, 2, 2x, 1, 1x, 0, 0x, head] 22");
 }
 }
